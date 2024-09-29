c4a0398cb9c7087e05c01ae0c5eda6ac92df2c63
Etai Nadler <75544136+etainad008@users.noreply.github.com>
2024-09-29 21:24:34 +0300

    Add script to insert chains to DB

---
+++ api/populate_database.py
@@ -0,0 +1,21 @@
+from constants import *
+from database import Database
+
+
+def populate():
+    populate_chains()
+
+
+def populate_chains():
+    values = [(CHAINS_DATA[chain]["id"], chain.name) for chain in CHAINS_DATA.keys()]
+    with Database() as db:
+        db.execute_many(
+            """
+            INSERT INTO Chain VALUES (%s, %s);
+            """,
+            values,
+        )
+
+
+if __name__ == "__main__":
+    populate()
