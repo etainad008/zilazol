27502e849767382f57870066c9ca6809c554ab2f
Etai Nadler <75544136+etainad008@users.noreply.github.com>
2024-10-03 21:32:58 +0300

    [Homepage]: Create top categories selection thingy

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,293 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<div dir="rtl">
+	<section>
+		<nav class="nav">
+			<p>שם ולוגו</p>
+			<section class="links">
+				<a href="/" class="link">בית</a>
+				<a href="/" class="link">לתחילת קנייה</a>
+				<a href="/" class="link">צרו קשר</a>
+			</section>
+			<div class="side-nav">
+				<img class="icon" src="/cart-icon.svg" alt="" />
+				<button class="signup">הרשמו</button>
+			</div>
+		</nav>
+		<hr class="hr" />
+	</section>
+
+	<main class="main">
+		<section class="hero">
+			<img src="/bijo.jpg" alt="" class="img-hero" />
+		</section>
+		<section class="cards">
+			<div>
+				<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path
+						d="M4 14L9 19L20 8M6 8.88889L9.07692 12L16 5"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+				</svg>
+				<p>השוואת מחירים מהימנה</p>
+			</div>
+			<div>
+				<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path
+						d="M3 11.3C3 6.32949 7.02944 2.30005 12 2.30005C16.9706 2.30005 21 6.32949 21 11.3M3 11.3H5C6.10457 11.3 7 12.1955 7 13.3V15.3C7 16.4046 6.10457 17.3 5 17.3M3 11.3V16.3C3 16.8523 3.44772 17.3 4 17.3H5M21 11.3H19C17.8954 11.3 17 12.1955 17 13.3V15.3C17 16.4046 17.8954 17.3 19 17.3H20C20.5523 17.3 21 16.8523 21 16.3V11.3ZM5 17.3V18.3C5 19.4046 5.89543 20.3 7 20.3H9M9 20.3C9 21.1285 9.67157 21.8 10.5 21.8H11.5C12.3284 21.8 13 21.1285 13 20.3C13 19.4716 12.3284 18.8 11.5 18.8H10.5C9.67157 18.8 9 19.4716 9 20.3Z"
+						stroke-linecap="round"
+					/>
+				</svg>
+				<p>תמיכה 24/7</p>
+			</div>
+			<div>
+				<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path
+						d="M5.82333 6.00037C6.2383 6.36683 6.5 6.90285 6.5 7.5C6.5 8.60457 5.60457 9.5 4.5 9.5C3.90285 9.5 3.36683 9.2383 3.00037 8.82333M5.82333 6.00037C5.94144 6 6.06676 6 6.2 6H17.8C17.9332 6 18.0586 6 18.1767 6.00037M5.82333 6.00037C4.94852 6.00308 4.46895 6.02593 4.09202 6.21799C3.71569 6.40973 3.40973 6.71569 3.21799 7.09202C3.02593 7.46895 3.00308 7.94852 3.00037 8.82333M3.00037 8.82333C3 8.94144 3 9.06676 3 9.2V14.8C3 14.9332 3 15.0586 3.00037 15.1767M3.00037 15.1767C3.36683 14.7617 3.90285 14.5 4.5 14.5C5.60457 14.5 6.5 15.3954 6.5 16.5C6.5 17.0971 6.2383 17.6332 5.82333 17.9996M3.00037 15.1767C3.00308 16.0515 3.02593 16.531 3.21799 16.908C3.40973 17.2843 3.71569 17.5903 4.09202 17.782C4.46895 17.9741 4.94852 17.9969 5.82333 17.9996M5.82333 17.9996C5.94144 18 6.06676 18 6.2 18H17.8C17.9332 18 18.0586 18 18.1767 17.9996M21 15.1771C20.6335 14.7619 20.0973 14.5 19.5 14.5C18.3954 14.5 17.5 15.3954 17.5 16.5C17.5 17.0971 17.7617 17.6332 18.1767 17.9996M21 15.1771C21.0004 15.0589 21 14.9334 21 14.8V9.2C21 9.06676 21 8.94144 20.9996 8.82333M21 15.1771C20.9973 16.0516 20.974 16.5311 20.782 16.908C20.5903 17.2843 20.2843 17.5903 19.908 17.782C19.5311 17.9741 19.0515 17.9969 18.1767 17.9996M20.9996 8.82333C20.6332 9.2383 20.0971 9.5 19.5 9.5C18.3954 9.5 17.5 8.60457 17.5 7.5C17.5 6.90285 17.7617 6.36683 18.1767 6.00037M20.9996 8.82333C20.9969 7.94852 20.9741 7.46895 20.782 7.09202C20.5903 6.71569 20.2843 6.40973 19.908 6.21799C19.5311 6.02593 19.0515 6.00308 18.1767 6.00037M14 12C14 13.1046 13.1046 14 12 14C10.8954 14 10 13.1046 10 12C10 10.8954 10.8954 10 12 10C13.1046 10 14 10.8954 14 12Z"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+				</svg>
+				<p>100% החזר כספי</p>
+			</div>
+			<div>
+				<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path
+						d="M13 3H8.2C7.0799 3 6.51984 3 6.09202 3.21799C5.71569 3.40973 5.40973 3.71569 5.21799 4.09202C5 4.51984 5 5.0799 5 6.2V17.8C5 18.9201 5 19.4802 5.21799 19.908C5.40973 20.2843 5.71569 20.5903 6.09202 20.782C6.51984 21 7.0799 21 8.2 21H15.8C16.9201 21 17.4802 21 17.908 20.782C18.2843 20.5903 18.5903 20.2843 18.782 19.908C19 19.4802 19 18.9201 19 17.8V9M13 3L19 9M13 3V7.4C13 7.96005 13 8.24008 13.109 8.45399C13.2049 8.64215 13.3578 8.79513 13.546 8.89101C13.7599 9 14.0399 9 14.6 9H19M15 12.21C14.9318 12.2141 14.7357 12.21 14.6667 12.21C13.6422 12.21 12.7077 11.7524 12 11C11.2923 11.7524 10.3578 12.2099 9.33333 12.2099C9.26426 12.2099 9.06837 12.2141 9.00016 12.21C9.00016 12.21 9 12.9855 9 13.3979C9 15.6121 10.2748 17.4725 12 18C13.7252 17.4725 15 15.6121 15 13.3979C15 12.9855 15 12.21 15 12.21Z"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+				</svg>
+				<p>תשלום בטוח</p>
+			</div>
+		</section>
+		<section class="top-cat">
+			<p>קטגוריות פופולאריות</p>
+			<section class="cat-test">
+				<div class="cat">
+					<div class="grad"></div>
+					<img src="/veg.jpg" alt="vegitables" />
+					<p>ירקות</p>
+				</div>
+				<div class="cat">
+					<div class="grad"></div>
+					<img src="/blue2.jpg" alt="fruit" />
+					<p>פירות</p>
+				</div>
+				<div class="cat">
+					<div class="grad"></div>
+					<img src="/eggs.jpg" alt="dairy" />
+					<p>מוצרי חלב</p>
+				</div>
+				<div class="cat">
+					<div class="grad"></div>
+					<img src="/chicken.jpg" alt="meat" />
+					<p>מוצרי בשר</p>
+				</div>
+				<div class="cat">
+					<div class="grad"></div>
+					<img src="/wine.jpg" alt="wine" />
+					<p>יין</p>
+				</div>
+				<div class="cat">
+					<div class="grad"></div>
+					<img src="/bread.jpg" alt="bread" />
+					<p>דגנים</p>
+				</div>
+				<div class="cat">
+					<div class="grad"></div>
+					<img src="/candy.jpg" alt="candy" />
+					<p>מתוקים</p>
+				</div>
+			</section>
+		</section>
+	</main>
+</div>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap');
+
+	:global(*) {
+		padding: 0;
+		margin: 0;
+		box-sizing: border-box;
+	}
+
+	:global(img) {
+		max-width: 100%;
+	}
+
+	:root {
+		--accent: #a3ea28;
+		font-family: 'Fredoka', sans-serif;
+	}
+
+	.nav {
+		display: flex;
+		justify-content: space-between;
+		padding: 0.5rem 1rem;
+		align-items: center;
+	}
+
+	.links {
+		display: flex;
+		gap: 2.5rem;
+		align-items: center;
+	}
+
+	.side-nav {
+		display: flex;
+		gap: 1rem;
+		align-items: center;
+	}
+
+	.link {
+		text-decoration: none;
+		color: black;
+	}
+
+	.icon {
+		width: 32px;
+		aspect-ratio: 1;
+	}
+
+	.signup {
+		padding: 0.5em 1em;
+		background-color: var(--accent);
+		border-style: none;
+		border-radius: 5px;
+	}
+
+	.signup:hover {
+		background-color: #91d122;
+		transition: 0.4s;
+		cursor: pointer;
+	}
+
+	.hr {
+		margin-inline: 1rem;
+		border: none;
+		border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+	}
+
+	main {
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+		align-items: center;
+		gap: 3rem;
+	}
+
+	.hero {
+		width: fit-content;
+		margin-inline: auto;
+		gap: 4rem;
+		margin-top: 5rem;
+		max-width: 100rem;
+	}
+
+	.hero > img {
+		border-radius: 20px;
+		object-fit: cover;
+		grid-column: span 2;
+		height: 18rem;
+	}
+
+	.cards {
+		display: flex;
+		justify-content: space-between;
+		width: 60%;
+		max-width: 50rem;
+		gap: 5rem;
+		margin-top: 5rem;
+	}
+
+	.cards > * {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+
+		& svg {
+			margin-bottom: 1rem;
+			width: 5rem;
+			stroke: #91d122;
+			stroke-width: 2px;
+		}
+	}
+
+	.top-cat {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin-bottom: 25rem;
+	}
+
+	.cat-test {
+		display: flex;
+		justify-content: center;
+		gap: 2rem;
+		flex-wrap: wrap;
+	}
+
+	.cat {
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-template-rows: 1fr;
+		align-items: end;
+		justify-items: start;
+		border-radius: 0.75rem;
+		text-align: center;
+		overflow: hidden;
+		box-shadow: rgba(0, 0, 0, 0.25) 0 0.5rem 1rem;
+	}
+
+	.cat:hover {
+		cursor: pointer;
+
+		& img {
+			scale: 1.05;
+		}
+	}
+
+	.grad {
+		background-image: linear-gradient(
+			to top,
+			rgba(0, 0, 0, 0.75),
+			rgba(0, 0, 0, 0.15),
+			transparent
+		);
+		grid-area: 1 / 1;
+		z-index: 1;
+		overflow: hidden;
+		width: 100%;
+		height: 100%;
+	}
+
+	.cat > img {
+		position: relative;
+		aspect-ratio: 1 / 1.333;
+		grid-area: 1 / 1;
+		max-width: 15rem;
+		object-fit: cover;
+		transition: scale 0.15s cubic-bezier(0, 0.5, 0.5, 1);
+	}
+
+	.cat > p {
+		grid-area: 1 / 1;
+		z-index: 2;
+		color: white;
+		font-weight: 500;
+		padding: 0.75rem;
+		font-size: 1.75em;
+	}
+
+	.top-cat > p {
+		margin-bottom: 2rem;
+		font-size: 2em;
+	}
+</style>
