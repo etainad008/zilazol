5c1e01756c380d4b284b6cbadf90939412e5b04b
Etai Nadler <75544136+etainad008@users.noreply.github.com>
2024-10-04 19:10:41 +0300

    [Homepage]: Move navbar to top layout file

---
+++ web/src/routes/+layout.svelte
@@ -0,0 +1,93 @@
+<script>
+	let buttonIsClicked = $state(false);
+
+	const handleClick = () => {
+		buttonIsClicked = !buttonIsClicked;
+	};
+</script>
+
+<div dir="rtl">
+	<section>
+		<nav class="nav">
+			<p>שם ולוגו</p>
+			<section class="links">
+				<a href="/" class="link">בית</a>
+				<a href="/" class="link">לתחילת קנייה</a>
+				<a href="/" class="link">צרו קשר</a>
+			</section>
+			<div class="side-nav">
+				<img class="icon" src="/cart-icon.svg" alt="" />
+				<a href="/login" class="signup link">הרשמו</a>
+			</div>
+		</nav>
+		<hr class="hr" />
+	</section>
+	<slot />
+</div>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap');
+
+	:global(*) {
+		padding: 0;
+		margin: 0;
+		box-sizing: border-box;
+	}
+
+	:global(img) {
+		max-width: 100%;
+	}
+
+	:root {
+		--accent: #a3ea28;
+		font-family: 'Fredoka', sans-serif;
+	}
+
+	.nav {
+		display: flex;
+		justify-content: space-between;
+		padding: 0.5rem 1rem;
+		align-items: center;
+	}
+
+	.links {
+		display: flex;
+		gap: 2.5rem;
+		align-items: center;
+	}
+
+	.side-nav {
+		display: flex;
+		gap: 1rem;
+		align-items: center;
+	}
+
+	.link {
+		text-decoration: none;
+		color: black;
+	}
+
+	.icon {
+		width: 32px;
+		aspect-ratio: 1;
+	}
+
+	.signup {
+		padding: 0.5em 1em;
+		background-color: var(--accent);
+		border-style: none;
+		border-radius: 5px;
+	}
+
+	.signup:hover {
+		background-color: #91d122;
+		transition: 0.4s;
+		cursor: pointer;
+	}
+
+	.hr {
+		margin-inline: 1rem;
+		border: none;
+		border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+	}
+</style>
